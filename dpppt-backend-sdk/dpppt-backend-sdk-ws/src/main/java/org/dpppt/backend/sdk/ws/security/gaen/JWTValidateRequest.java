480ab038333fb4264622843e3f05dea52539276d
mapalomar <mapalomar@minsait.com>
2020-10-09 09:57:52 +0200

    [MAPR] Fix - Apps are sending fake TEKs for more days than expected

--- dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/security/gaen/JWTValidateRequest.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/security/gaen/JWTValidateRequest.java
@@ -10,11 +10,6 @@
 
 package org.dpppt.backend.sdk.ws.security.gaen;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-
 import org.apache.commons.lang3.StringUtils;
 import org.dpppt.backend.sdk.model.gaen.GaenKey;
 import org.dpppt.backend.sdk.model.gaen.GaenUnit;
@@ -25,85 +20,102 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
 public class JWTValidateRequest implements ValidateRequest {
 
-	private static final Logger logger = LoggerFactory.getLogger(JWTValidateRequest.class);
+    private static final Logger logger = LoggerFactory.getLogger(JWTValidateRequest.class);
 
-	private static final String CLAIM_SCOPE = "scope";
-	private static final String CLAIM_TAN = "tan";
+    private static final String CLAIM_SCOPE = "scope";
+    private static final String CLAIM_TAN = "tan";
 
     @Value("${application.endpoint.validation.enabled:true}")
     private boolean validationEnabled;
 
-	private final ValidationClientService validationClientService;
+    // FIXME Quitar
+    @Value("${application.jwt.validate.fake.enabled:true}")
+    private boolean fakeEnabled;
 
-	public JWTValidateRequest(ValidationClientService validationClientService) {
-		this.validationClientService = validationClientService;
-	}
+    private final ValidationClientService validationClientService;
 
-	@Override
-	public boolean isValid(Object authObject) {
-		if (authObject instanceof Jwt) {
-			Jwt token = (Jwt) authObject;
-			boolean checkTan = checkTanClaim(token);
-			logger.info("checkTan = {}", checkTan);
-			return token.containsClaim(CLAIM_SCOPE) && token.getClaim(CLAIM_SCOPE).equals("exposed") && checkTan;
-		}
-		return false;
-	}
+    public JWTValidateRequest(ValidationClientService validationClientService) {
+        this.validationClientService = validationClientService;
+    }
 
-	private boolean checkTanClaim(Jwt token) {
-		boolean result = true;
-		if (validationEnabled && !isFakeToken(token)) {
-			String tan = token.containsClaim(CLAIM_TAN) ? (String) token.getClaim(CLAIM_TAN) : "";
-			result = StringUtils.isNoneEmpty(tan) 
-					&& validationClientService.validate(tan);
-		}
-		return result;
-	}
+    @Override
+    public boolean isValid(Object authObject) {
+        if (authObject instanceof Jwt) {
+            Jwt token = (Jwt) authObject;
+            boolean checkTan = checkTanClaim(token);
+            logger.info("checkTan = {}", checkTan);
+            return token.containsClaim(CLAIM_SCOPE) && token.getClaim(CLAIM_SCOPE).equals("exposed") && checkTan;
+        }
+        return false;
+    }
 
-	@Override
-	public long getKeyDate(Object authObject, Object others) throws InvalidDateException {
-		if (authObject instanceof Jwt) {
-			Jwt token = (Jwt) authObject;
-			long jwtKeyDate = LocalDate.parse(token.getClaim("onset")).atStartOfDay().atOffset(ZoneOffset.UTC).toInstant().toEpochMilli();
-			if (others instanceof GaenKey) {
+    private boolean checkTanClaim(Jwt token) {
+        boolean result = true;
+        if (validationEnabled && !isFakeToken(token)) {
+            String tan = token.containsClaim(CLAIM_TAN) ? (String) token.getClaim(CLAIM_TAN) : "";
+            result = StringUtils.isNoneEmpty(tan) && validationClientService.validate(tan);
+        }
+        return result;
+    }
+
+    @Override
+    public long getKeyDate(Object authObject, Object others) throws InvalidDateException {
+        if (authObject instanceof Jwt) {
+            Jwt token = (Jwt) authObject;
+            long jwtKeyDate = LocalDate.parse(token.getClaim("onset")).atStartOfDay().atOffset(ZoneOffset.UTC).toInstant().toEpochMilli();
+            if (others instanceof GaenKey) {
                 GaenKey request = (GaenKey) others;
                 var keyDate = Duration.of(request.getRollingStartNumber(), GaenUnit.TenMinutes);
-				if (keyDate.toMillis() > System.currentTimeMillis()) {
-					throw new InvalidDateException();
-				} else if (keyDate.toMillis() < jwtKeyDate) {
-					throw new InvalidDateException();
-				} 
-				else if(keyDate.toMillis() < OffsetDateTime.now().minusDays(21).toInstant().toEpochMilli()) {
-					throw new InvalidDateException();
-				}
-				jwtKeyDate = keyDate.toMillis();
-			}
-			return jwtKeyDate;
-		}
-		throw new IllegalArgumentException();
-	}
+                if (keyDate.toMillis() > System.currentTimeMillis()) {
+                    throw new InvalidDateException("keyDate > currentTimeMillis");
+                } else if (keyDate.toMillis() < jwtKeyDate) {
+                    throw new InvalidDateException("keyDate < onset");
+                } else if (keyDate.toMillis() < OffsetDateTime.now().minusDays(21).toInstant().toEpochMilli()) {
+                    throw new InvalidDateException("keyDate < now - 21 days");
+                }
+                jwtKeyDate = keyDate.toMillis();
+            }
+            return jwtKeyDate;
+        }
+        throw new IllegalArgumentException();
+    }
 
-	@Override
-	public boolean isFakeRequest(Object authObject, Object others) {
-		if (authObject instanceof Jwt && others instanceof GaenKey) {
-			Jwt token = (Jwt) authObject;
-			GaenKey request = (GaenKey) others;
-			boolean fake = false;
-			if (isFakeToken(token)) {
-				fake = true;
-			}
-			if (request.getFake() == 1) {
-				fake = true;
-			}
-			return fake;
-		}
-		throw new IllegalArgumentException();
-	}
+    @Override
+    public boolean isFakeRequest(Object authObject, Object others) {
+        if (authObject instanceof Jwt && others instanceof GaenKey) {
+            Jwt token = (Jwt) authObject;
+            GaenKey request = (GaenKey) others;
+            boolean fake = false;
+            if (isFakeToken(token)) {
+                fake = true;
+            }
+            if (request.getFake() == 1) {
+                fake = true;
+            }
 
-	private boolean isFakeToken(Jwt token) {
-		return token != null && (token.containsClaim("fake") && token.getClaimAsString("fake").equals("1"));
-	}
+            // FIXME: Quitar una vez que apps desplieguen nueva versión
+            if (fakeEnabled && !fake && token.containsClaim("onset") && token.getClaim("onset") != null) {
+                long jwtKeyDate = LocalDate.parse(token.getClaim("onset")).minusDays(1).atStartOfDay().atOffset(ZoneOffset.UTC).toInstant().toEpochMilli();
+                var keyDate = Duration.of(request.getRollingStartNumber(), GaenUnit.TenMinutes);
+                if (keyDate.toMillis() <= jwtKeyDate) {
+                    fake = true;
+                }
+            }
+
+            return fake;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private boolean isFakeToken(Jwt token) {
+        return token != null && (token.containsClaim("fake") && token.getClaimAsString("fake").equals("1"));
+    }
 
 }
